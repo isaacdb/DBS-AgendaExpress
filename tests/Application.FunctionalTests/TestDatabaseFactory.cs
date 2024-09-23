590aa2ec948967883080394c8a0aded822367f6d
isaacdbs@outlook.com <isaacdbs@outlook.com>
2024-09-23 19:30:56 -0300

    inicio

---
+++ tests/Application.FunctionalTests/TestDatabaseFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace YourProjectName.Application.FunctionalTests;
+
+public static class TestDatabaseFactory
+{
+    public static async Task<ITestDatabase> CreateAsync()
+    {
+        var database = new TestcontainersTestDatabase();
+
+        await database.InitialiseAsync();
+
+        return database;
+    }
+}
